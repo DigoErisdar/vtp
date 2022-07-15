1f2e6377b0dfe9d117ec0eae67ba532687d47f40
DigoErisdar <kingofthedemons@mail.ru>
2022-07-15 19:06:29 +0300

    add filters and sorting

--- drf/apps/employees/views/employees.py
+++ drf/apps/employees/views/employees.py
@@ -1,4 +1,6 @@
+import django_filters
 from rest_framework import viewsets
+from rest_framework import filters
 
 from apps.employees.serializers.employees import EmployeeSerializer
 
@@ -6,3 +8,8 @@ from apps.employees.serializers.employees import EmployeeSerializer
 class EmployeeViewSet(viewsets.ModelViewSet):
     serializer_class = EmployeeSerializer
     queryset = serializer_class.Meta.model.objects.all()
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['isArchive', 'role']
+    search_fields = ['name']
+    ordering_fields = ['name']
+    ordering = ['name']
